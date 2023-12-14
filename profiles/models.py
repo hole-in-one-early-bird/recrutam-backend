fb7e49c1991ce777e3b9b8abe3ebaae935a6afe3
swanim <stellarswan20@gmail.com>
2023-12-15 00:16:15 +0900

    feat : profiles/info2 POST

--- profiles/models.py
+++ profiles/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class UserProfile(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
     gender = models.CharField(max_length=10)
     age = models.IntegerField()
@@ -8,3 +9,14 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class UserInterest(models.Model):
+    id = models.AutoField(primary_key=True)
+    user_id = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    interest1 = models.CharField(max_length=255)
+    interest2 = models.CharField(max_length=255)
+    interest3 = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"User: {self.user_id.name}, Interests: {self.interest1}, {self.interest2}, {self.interest3}"
--- profiles/urls.py
+++ profiles/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import UserProfileView
+from .views import *
 
 urlpatterns = [
     path('info1/', UserProfileView.as_view(), name='info1'),
+    path('info2/', UserInterestView.as_view(), name='info2'),
 ]
