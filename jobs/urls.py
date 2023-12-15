73dcfaf06f6fa5b7423b41bbf2bb18868fc750bd
swanim <stellarswan20@gmail.com>
2023-12-16 00:23:33 +0900

    feat : jobs/recommendations/ POST

--- jobs/urls.py
+++ jobs/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from .views import *
 
 urlpatterns = [
-    
+    path('recommendations/', SyncProfileDataView.as_view(), name='recommendations'),
+    #path('recommendations/', your_view_function, name='recommendations'),
+    #path('completion/', completion_request_view, name='completion-request'),
 ]
--- recrutam/urls.py
+++ recrutam/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('profiles/', include('profiles.urls')),
+    path('jobs/', include('jobs.urls')),
 ]
