dbbc2f4217ec6fd0eac5a99ce5fe3c9b486f7327
swanim <stellarswan20@gmail.com>
2023-12-16 10:30:15 +0900

    feat : /loading GET

--- home/views.py
+++ home/views.py
@@ -3,3 +3,6 @@ from django.http import JsonResponse
 
 def home(request):
     return JsonResponse({'message': 'Welcome to the Home Page!'})
+
+def loading(request):
+    return JsonResponse({'message': 'loading page'})
